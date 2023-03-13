993d1b8fa0f8e49e0442e851a054011029332efc
baguspradita12 <baguspradita12@student.uns.ac.id>
2023-03-13 20:16:20 +0700

    menambah tugas 1

---
+++ Exercise-01.py
@@ -0,0 +1,7 @@
+print("Bagus Pradita")
+print(" ")
+print("I0322022")
+print(" ")
+print("Hallo perkenalkan nama saya Bagus Pradita asal saya dari Kebumen, Jawa Tengah. Cukup jauh dengan kota Solo membutuhkan perjalanan sekitar 5 jam dari Kebumen, perbedaan budaya yang saya alami dari kota Kebumen dengan kota solo tidak terlalu jauh berbeda hanya saja terdapat perbedaan di bahasa jika diKebumen kami menggunakan bahasa ngapak. Saya tinggal di pedesaan yang kampung halamannya masih hijau belum terlalu rapat akan gedung-gedung tinggi, rumah saya cukup jauh dari perkotaan sekitar 20 menit untuk menempuh perjalanan ke pusat kota, kota saya cukup terkenal akan pantainya yang indah seperti pantai Menganti, dan terdapat makanan khas kebumen yaitu lanting")
+print(" ")
+print("Harapan saya sabagai lulusan Teknik Industri Universitas Sebelas Maret saya bisa diterima kerja yang sesuai dengan kemauan saya, bisa berguna bagi masyarakat sekitar dan saya ingin memiliki usaha Vape Store")
